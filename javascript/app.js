ba240f9c4e548ccb80577ea10bbfdce37b7360c5
Tony Vu <52386267+tonyvudev@users.noreply.github.com>
2019-10-17 21:19:47 -0500

    Added scrollable homepage pictures

--- javascript/app.js
+++ javascript/app.js
@@ -752,8 +752,51 @@ window.onscroll = function () {
 
 		}
 	}
-	
+
 };
+// END
+/************************************************************************************** */
+
+/************************************************************************************** */
+// Scrollable homepage pics
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+	isDown = true;
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+
+
+});
+
+slider.addEventListener('mouseleave', () => {
+	isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+	isDown = false;
+});
+
+
+slider.addEventListener('mousemove', (e) => {
+	if (!isDown) return;
+
+	e.preventDefault(); // prevents links or
+	// other actions from happening
+
+	const x = e.pageX - slider.offsetLeft;
+
+	const walk = (x - startX) * 1; //scroll-fast
+
+	slider.scrollLeft = scrollLeft - walk;
+
+
+});
+
+slider.scrollLeft = 725;
 // END
 /************************************************************************************** */
 
