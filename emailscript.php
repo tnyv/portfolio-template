233152186cf2983a2623af7919285a3a0de755cb
Tony Vu <52386267+tonyvudev@users.noreply.github.com>
2019-10-26 00:07:44 -0500

    Added php for email form

--- emailscript.php
+++ emailscript.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Idk</title>
-    </head>
 
+<body>
+    <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = "Name: " . $name . "\r\n" . "Email: " . $email . "\r\n" .
+    "Subject: " . $subject . "\r\n" . "Message: " . $_POST['message'];
 
-    <body>
-        <?php
-            echo "<p>YOOO</>";
-        ?>
-    </body>
+    $to = "contact@tonyvu.io";
+
+    $headers = "From:user@domain.com";
+
+    $sent = mail($to, $subject, $message, $headers);
+
+    if($sent){
+        echo "<span style='color:green; font-weight:bold;'>
+        Thank you for contacting us, we will get back to you shortly.
+        </span>";
+    }
+    else{
+        echo "<span style='color:red; font-weight:bold;'>
+        Sorry! Your form submission is failed.
+        </span>";
+        }
+    
+    ?>
+</body>
+
 </html>
